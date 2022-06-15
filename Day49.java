60a8c1be7a3070f9049d7f5dd4757048cfe3ee98
tvenkataramana570 <83851416+tvenkataramana570@users.noreply.github.com>
2022-06-15 12:03:16 +0530

    Create Day49.java

---
+++ Day49.java
@@ -0,0 +1,39 @@
+/*
+
+This problem was asked by Amazon.
+
+Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+
+For example, given the array [34, -50, 42, 14, -5, 86], the maximum sum would be 137, since we would take elements 42, 14, -5, and 86.
+
+Given the array [-5, -1, -8, -9], the maximum sum would be 0, since we would not take any elements.
+
+Do this in O(N) time.
+
+*/
+import java.util.*;
+
+class Main
+{
+	public static void main (String[] args)
+	{
+		int [] a = {-2, -3, 4, -1, -2, 1, 5, -3};
+		System.out.println("Maximum contiguous sum is "+maxSubArraySum(a));
+	}
+
+	static int maxSubArraySum(int a[])
+	{
+		int size = a.length;
+		int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+
+		for (int i = 0; i < size; i++)
+		{
+			max_ending_here = max_ending_here + a[i];
+			if (max_so_far < max_ending_here)
+				max_so_far = max_ending_here;
+			if (max_ending_here < 0)
+				max_ending_here = 0;
+		}
+		return max_so_far;
+	}
+}
